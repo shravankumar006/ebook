61d59f1f9539fd4e7b6a1b92768be5a4b105f129
Shravan <shravan1458119@gmail.com>
2024-08-03 03:21:44 +0530

    link updated

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4000/user/login", userInfo)
+      .post("https://ebook-jvbc.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
